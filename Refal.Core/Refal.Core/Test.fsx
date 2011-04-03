fe3fc0c2e3b910e308f58e1875abef5f70ac871d
dk.kravchuk@gmail.com <dk.kravchuk@gmail.com@d963916d-1093-8880-f374-b92f9f2b7933>
2011-04-03 08:48:27 +0000

    Initial commit.

---
+++ Refal.Core/Refal.Core/Test.fsx
@@ -0,0 +1,13 @@
+﻿open System
+
+#load "ParserCombinator.fs"
+#load "RefalParser.fs"
+#load "Refal.fs"
+
+open Refal.ParserCombinator
+open Refal.RefalParser
+open Refal.Refal
+
+let pat = (many term) (Seq.toList "'abcd' s.1 'f' s.1") |> List.head |> fst |> List.concat
+let inp = Seq.toList "abcdefe"
+let x = matchInp pat inp
